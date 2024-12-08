694c9db0190dd778b557a354e5fdb95c6976fca1
tanujjh123 <41846646+tanujjh123@users.noreply.github.com>
2024-12-08 13:20:27 +0530

    Add files via upload

---
+++ Basic_codes.py
@@ -0,0 +1,14 @@
+# Primitive data types
+companyName = "ETL QA Labs"
+print(companyName, " :: ", type(companyName))
+noOfEmployees = 100
+print(noOfEmployees, " :: ", type(noOfEmployees))
+turnover = 50.5
+print(turnover, " :: ", type(turnover))
+elligibleForVoting = False
+print(elligibleForVoting, " :: ", type(elligibleForVoting))
+# list
+employeeNames = ["Ram", "Shyam", "Ghanshyam"]
+print(employeeNames," :: ",type(employeeNames))
+items = ["washingmachine", "clothes", 1, 5, True]
+print(items," :: ",type(items))
